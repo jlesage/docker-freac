12b218a90ee7b1dd32bbbb78058145ecdb5c3557
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-23 13:43:37 -0400

    Added `/storage` to bookmarks of the open file window.

--- rootfs/etc/cont-init.d/55-freac.sh
+++ rootfs/etc/cont-init.d/55-freac.sh
@@ -5,9 +5,11 @@ set -u # Treat unset variables as an error.
 
 # Make sure the configuration directory exists.
 mkdir -p "$XDG_CONFIG_HOME"/.freac
+mkdir -p "$XDG_CONFIG_HOME"/gtk-3.0
 
 # Install default configuration.
 [ -f "$XDG_CONFIG_HOME"/.freac/freac.xml ] || cp -v /defaults/freac.xml "$XDG_CONFIG_HOME"/.freac/freac.xml
+[ -f "$XDG_CONFIG_HOME"/gtk-3.0/bookmarks ] || cp -v /defaults/bookmarks "$XDG_CONFIG_HOME"/gtk-3.0/bookmarks
 
 # Take ownership of the output directory.
 take-ownership --not-recursive /output
